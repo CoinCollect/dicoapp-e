fe2701143d7eae60344115608ff065b137a08d0e
bartwr <mail@bartroorda.nl>
2018-12-20 17:39:19 +0100

    Fixed BTC explorer URL

--- app/containers/WalletPage/components/Transactions.js
+++ app/containers/WalletPage/components/Transactions.js
@@ -55,7 +55,7 @@ const styles = () => ({
 
 const explorer = {
   KMD: 'http://kmdexplorer.io/tx',
-  BTC: 'https://blockchain.com/tx',
+  BTC: 'https://blockchain.com/btc/tx',
   LTC: 'https://live.blockcypher.com/ltc/tx',
   GLXT: 'http://glx.info/tx'
 };
