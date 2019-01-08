8246235ec882770439bf22be500cdc7d88bcbd02
bartwr <mail@bartroorda.nl>
2019-01-08 22:40:51 +0100

    Improved BuyPage UX by adding informative text

--- app/containers/BuyPage/components/PaymentSection.js
+++ app/containers/BuyPage/components/PaymentSection.js
@@ -84,17 +84,19 @@ class PaymentSection extends PureComponent<Props> {
     if (!c) {
       // not found in entities
       return (
-        <CoinSelectable
-          dispatchLoadPrice={dispatchLoadPrice}
-          disabled
-          key={`paymentCoin${symbol}`}
-          data={symbol}
-          icon={icon}
-          title={name}
-          subTitle={`${floor(b.get('balance'), 3)} ${b.get('coin')}`}
-        >
-          {lineContent}
-        </CoinSelectable>
+        <div>
+          <CoinSelectable
+            dispatchLoadPrice={dispatchLoadPrice}
+            disabled
+            key={`paymentCoin${symbol}`}
+            data={symbol}
+            icon={icon}
+            title={name}
+            subTitle={`${floor(b.get('balance'), 3)} ${b.get('coin')}`}
+          >
+            {lineContent}
+          </CoinSelectable>
+        </div>
       );
     }
     return (
@@ -128,7 +130,52 @@ class PaymentSection extends PureComponent<Props> {
     if (loading && list.size === 0) {
       return this.renderLoading();
     }
-    return list.map(this.renderPaymentCoin);
+
+    // Get entities
+    const { entities } = this.props;
+    const cKMD = entities.get('KMD');
+
+    // Show info text as long as KMD order book is not ready
+    const showInfoText = !cKMD || cKMD.get('bestPrice') === 0;
+
+    return (
+      <div>
+        <div
+          style={{
+            fontSize: '16px',
+            width: 'calc(100% - 20px)',
+            display: showInfoText ? 'block' : 'none'
+          }}
+        >
+          <p>
+            You will buy CC using a <b>d</b>
+            ICO app. This means you buy your coins safely, peer to peer, without
+            a third party in between.
+          </p>
+          <p>
+            Wait a few moments so that you can start buying the coins from this
+            decentralized network. The coins below will become clickable in less
+            then 1 minute.
+          </p>
+        </div>
+        <div>
+          <p
+            style={{
+              fontWeight: 'normal',
+              fontSize: '16px',
+              width: 'calc(100% - 20px)',
+              display: showInfoText ? 'none' : 'block'
+            }}
+          >
+            You can buy CoinCollect with KMD or BTC. KMD swaps are fast & cheap.
+            BTC swaps are slower and have a higher transaction fee. We recommend
+            to use BTC for bigger transaction amounts only.
+          </p>
+        </div>
+        <br />
+        {list.map(this.renderPaymentCoin)}
+      </div>
+    );
   }
 }
 
